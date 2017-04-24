efc68b858aabf62221128f1ca5ec750c81a8ab89
adaprojectnet <adaprojectnet@gmail.com>
2017-04-24 17:14:45 +1000

    My First Commit

---
+++ js/htmlcommentbox.js
@@ -0,0 +1,2 @@
+// JavaScript Document
+ /*<!--*/ if(!window.hcb_user){hcb_user={  };} (function(){s=document.createElement("script");s.setAttribute("type","text/javascript");s.setAttribute("src", "http://www.htmlcommentbox.com/jread?page="+escape((window.hcb_user && hcb_user.PAGE)||(""+window.location)).replace("+","%2B")+"&opts=470&num=10");if (typeof s!="undefined") document.getElementsByTagName("head")[0].appendChild(s);})(); /*-->*/ 
